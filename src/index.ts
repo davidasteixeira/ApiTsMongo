919e0594183f050f0beecc391dde71ab2a37d1ca
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2023-08-01 23:19:43 -0300

    feat: add express basic server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello world")
+import express from 'express';
+import 'dotenv/config';
+
+const app = express()
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}!`);
+});
+
+app.get('/', (req, res) => {
+    res.send("Hello world")
+})
