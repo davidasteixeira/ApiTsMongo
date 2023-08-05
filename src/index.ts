8ec5b50da8b45741c41299c8851b9ba92f84d9d9
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2023-08-05 20:09:14 -0300

    feat: add MongoGetUsersRepository

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import { MongoGetUsersRepository } from './repositories/get-users/mongo-get-users';
 import express from 'express';
 import 'dotenv/config';
+import { GetUsercontroller } from './controllers/get-users/get-users';
 
 const app = express()
 
@@ -11,4 +13,13 @@ app.listen(port, () => {
 
 app.get('/', (req, res) => {
     res.send("Hello world")
+});
+
+app.get('/users', async (req, res) => {
+    const mongoGetUsersRepository = new MongoGetUsersRepository();
+
+    const getUsersCOntroller = new GetUsercontroller(mongoGetUsersRepository);
+
+    const { body, statusCode } = await getUsersCOntroller.handle();
+    res.send(body).status(statusCode);
 })
